15a201bc3dbb5ae4e057b9f0d8adaed3aa172206
Khalid Hersafril <khalidowlwalid@gmail.com>
2025-06-01 15:01:02 +0100

    [fix] Fix a bug where the mavlink packets were the wrong size

--- mavlink_listener.cpp
+++ mavlink_listener.cpp
@@ -2,6 +2,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <mavlink/common/mavlink.h>
+#include <time.h>
 
 #define print(msg) std::cout << msg;
 #define print_line(msg) std::cout << msg << std::endl;
@@ -9,6 +10,11 @@
 #define SERVER "127.0.0.1" // ip-адрес сервера (локальный хост)
 #define BUFLEN 2048 // максимальная длина ответа
 #define PORT 14550 // порт для приема данных
+
+static time_t last_time = 0;
+
+struct sockaddr_in source_addr = {};
+socklen_t source_addr_len = sizeof(source_addr);
 
 class UDPClient {
 public:
@@ -24,10 +30,15 @@ public:
             exit(EXIT_FAILURE);
         }
 
-        memset((char*)&server_address, 0, sizeof(server_address));
-        server_address.sin_family = AF_INET;
-        server_address.sin_port = htons(PORT);
-        server_address.sin_addr.S_un.S_addr = inet_addr(SERVER);
+        struct sockaddr_in server_addr;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(PORT);
+        server_addr.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+        if (bind(client_socket_fd, (const sockaddr *)(&server_addr), sizeof(server_addr)) != 0) {
+            print_line("Bind error");
+            exit(EXIT_FAILURE);
+        }
     }
 
     ~UDPClient() {
@@ -35,19 +46,70 @@ public:
         WSACleanup();
     }
 
+    void send_heartbeat() {
+
+        mavlink_message_t message;
+        const uint8_t system_id = 42;
+        const uint8_t base_mode = 0;
+        const uint8_t custom_mode = 0;
+
+        mavlink_msg_heartbeat_pack_chan(
+            system_id,
+            MAV_COMP_ID_PERIPHERAL,
+            MAVLINK_COMM_0,
+            &message,
+            MAV_TYPE_GENERIC,
+            MAV_AUTOPILOT_GENERIC,
+            base_mode,
+            custom_mode,
+            MAV_STATE_STANDBY
+        );
+
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        const int len = mavlink_msg_to_send_buffer(buffer, &message);
+
+        sendto(client_socket_fd, (const char *)buffer, strlen((const char *)buffer), 0, (sockaddr*)&source_addr, source_addr_len); 
+        // if (sendto(client_socket_fd, (const char *)buffer, strlen((const char *)buffer), 0, (sockaddr*)&source_addr, source_addr_len) == SOCKET_ERROR) {
+        //     print_line("Failed to send from client");
+        //     exit(EXIT_FAILURE);
+        // }
+    }
+
+    // void send_heartbeat()
+    // {
+        // mavlink_message_t message;
+
+        // const uint8_t system_id = 42;
+        // const uint8_t base_mode = 0;
+        // const uint8_t custom_mode = 0;
+        // mavlink_msg_heartbeat_pack_chan(
+        //     system_id,
+        //     MAV_COMP_ID_PERIPHERAL,
+        //     MAVLINK_COMM_0,
+        //     &message,
+        //     MAV_TYPE_GENERIC,
+        //     MAV_AUTOPILOT_GENERIC,
+        //     base_mode,
+        //     custom_mode,
+        //     MAV_STATE_STANDBY);
+
+    //     uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+    //     const int len = mavlink_msg_to_send_buffer(buffer, &message);
+
+    //     int ret = sendto(client_socket_fd, (const char *)buffer, len, 0, (sockaddr*)&source_addr, source_addr_len);
+    //     if (ret != len) {
+    //         printf("sendto error: %s\n", strerror(errno));
+    //     } else {
+    //         printf("Sent heartbeat\n");
+    //     }
+    // }
+
     void start() {
         while (true) {
-            char buffer[BUFLEN] = {};
-            int server_addr_size = sizeof(server_address);
-            char message[200] = "Hello world from the client";
-
-            if (sendto(client_socket_fd, message, strlen(message), 0, (sockaddr*)&server_address, server_addr_size) == SOCKET_ERROR) {
-                print_line("Failed to send from client");
-                exit(EXIT_FAILURE);
-            }
 
             // Will return n bytes if successful
-            const int result = recvfrom(client_socket_fd, buffer, BUFLEN, 0, (sockaddr*)&server_address, &server_addr_size); 
+            char recv_buffer[2048];
+            const int result = recvfrom(client_socket_fd, recv_buffer, BUFLEN, 0, (sockaddr*)&source_addr, &source_addr_len); 
             if (result == SOCKET_ERROR) {
                 print("Failed to receive data from server with error: ");
                 print(WSAGetLastError());
@@ -55,11 +117,11 @@ public:
                 exit(EXIT_FAILURE);
             }
 
-            print("Received bytes: ");
-            print(result);
-            print_line("");
-
-
+            time_t current_time = time(NULL);
+            if (current_time - last_time >= 1) {
+                send_heartbeat();
+                last_time = current_time;
+            }
         }
     }
 
