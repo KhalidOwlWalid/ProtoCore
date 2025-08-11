744967e903a836710b9a2f2cb5613130f815ad3e
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-08-12 00:20:48 +0100

    [refactor] Create more helper function for mavlink examples, and refactor examples to be a bit more general

--- examples/basic/mavlink/mavlink_server.cpp
+++ examples/basic/mavlink/mavlink_server.cpp
@@ -1,33 +1,38 @@
+/*
+This example provides a basic guide on how you can create your own custom GCS by listening to the open by the autopilot.
+
+To test this script, on your terminal, run:
+sim_vehicle.py --vehicle Copter --out=127.0.0.1:14550
+
+Once the SITL is running, run this script:
+./mavlink_server
+
+Please note that since this binds with the localhost:14550 socket, having any Mission Planner or QGroundControl which already
+connects to this network will cause bind error. Please ensure you disconnect them before running this.
+*/
+
 #include "mavlink_common.hpp"
+
+int socket_fd;
+struct sockaddr_in server_address = {};
+struct sockaddr_in client_address = {};
+socklen_t client_address_len = sizeof(client_address);
 
 int main() {
 
     spdlog::info("Mavlink Server example");
 
-    // Setup file descriptor
-    // Use of UDP protocol
-    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    // This is compulsary if you are developing GCS as you need to bind to a socket
+    init_server(socket_fd, server_address, AF_INET, SOCK_DGRAM, "127.0.0.1", MAVLINK_SERVER_PORT);
 
-    if (sock_fd < 0) {
-        spdlog::error("Socket fails to be created with error, {}", strerror(errno));
-    } 
-
-    memset(&server_address, 0, sizeof(client_address));
-    server_address.sin_family = AF_INET;
-    inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
-    server_address.sin_port = htons(14550);
-
-    if (bind(sock_fd, (struct sockaddr*)(&server_address), sizeof(server_address)) < 0) {
-        spdlog::error("Bind failed with error, {}", strerror(errno));
-        close(sock_fd);
-        return 0;
-    }
-
-    // spdlog::info("Mavlink server listening on port {}:{}", server_address.sin
+    char ip_str[INET_ADDRSTRLEN];
+    spdlog::info("Mavlink server running on port {}:{}", \ 
+        inet_ntop(AF_INET, &(server_address.sin_addr), ip_str, INET_ADDRSTRLEN), ntohs(server_address.sin_port));
 
     while (run_application) {
 
-        ssize_t recv_len = recvfrom(sock_fd, buffer, 2048, 0, (struct sockaddr*)(&server_address), &address_len); 
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        ssize_t recv_len = recvfrom(socket_fd, buffer, 2048, 0, (struct sockaddr*)(&client_address), &client_address_len); 
 
         if (recv_len < 0) {
             spdlog::error("recvfrom failed with error, {}", strerror(errno));
@@ -36,37 +41,20 @@ int main() {
 
         mavlink_message_t mav_message;
         mavlink_status_t mav_status;
-        uint8_t some_val = 1;
-        
+
+        char ip_str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(client_address.sin_addr), ip_str, INET_ADDRSTRLEN);
+
         for (size_t i = 0; i < recv_len; i++) {
             if (mavlink_parse_char(MAVLINK_COMM_0, buffer[i], &mav_message, &mav_status)) {
                 switch (mav_message.msgid) {
                     case MAVLINK_MSG_ID_HEARTBEAT:
-                        spdlog::info("Receiving data from Heartbeat {}:{}", mav_message.sysid, mav_message.compid);
-                        std::cout << some_val << std::endl;
+                        spdlog::info("Receiving Heartbeat from [target:component][{}:{}] at address {}:{}", mav_message.sysid, mav_message.compid, ip_str, ntohs(client_address.sin_port));
                     case MAVLINK_MSG_ID_POSITION_TARGET_LOCAL_NED:
                         spdlog::info("Received data for target position");
                         mavlink_position_target_local_ned_t target_local_pos_ned;
                         mavlink_msg_position_target_local_ned_decode(&mav_message, &target_local_pos_ned);
                         spdlog::info("Target Local POS NED: {}, {}", target_local_pos_ned.x, target_local_pos_ned.y);
-                    case MAVLINK_MSG_ID_LOCAL_POSITION_NED:
-                        spdlog::info("Receiving data from Local Position NED with {} bytes.", recv_len);
-                        mavlink_local_position_ned_t local_pos_ned;
-                        mavlink_msg_local_position_ned_decode(&mav_message, &local_pos_ned);
-                        spdlog::info("Data received from NED: {}, {}, {}", local_pos_ned.x, local_pos_ned.y, local_pos_ned.z);
-                    case MAVLINK_MSG_ID_PARAM_REQUEST_READ:
-                        mavlink_param_request_read_t param_request;
-                        mavlink_msg_param_request_read_decode(&mav_message, &param_request);
-                        spdlog::info("Data: {}", param_request.param_index);
-                    case MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT:
-                        spdlog::info("Receiving data from Nav Controller Output with {} bytes.", recv_len);
-                        mavlink_nav_controller_output_t nav_controller_output;
-                        mavlink_msg_nav_controller_output_decode(&mav_message, &nav_controller_output);
-                        mavlink_message_interval_t mav_interval_t;
-                        mavlink_msg_message_interval_decode(&mav_message, &mav_interval_t);
-                        spdlog::info("Data received from Nav Controller Output: {}, {}, {} with interval of {} us",  nav_controller_output.nav_roll, nav_controller_output.nav_pitch, nav_controller_output.nav_bearing, mav_interval_t.interval_us);
-                    case MAVLINK_MSG_ID_PID_TUNING:
-                        spdlog::info("Receiving data from PID Tuning Output with {} bytes.", recv_len);
                     default:
                         #if PRINT_ALL_MSG_ID
                         spdlog::info("Receiveing data from: -");
@@ -79,7 +67,6 @@ int main() {
             }
         }
     }
-
-    close(sock_fd);
+    close(socket_fd);
     return 0;
 }
